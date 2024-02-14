af76205736ea797f7057dbc5c5c8dcc7b619e338
Roopesh-N <roopeshnagineni123@outlook.com>
2024-02-13 23:38:40 -0500

    second commit

--- todoproject/todoApp/urls.py
+++ todoproject/todoApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('homepageview/<slug:slug>/', views.homepage_view,name='homepage'),
-    path('login/',views.login_view,name='login'),
+    path('',views.login_view,name='login'),
     path('signup/',views.signup_view,name='signup'),
     path('addtask/<slug:slug>/',views.addtask_view,name='addtask'),
     path('updatetask/<slug:slug>/<str:title>',views.updatetask_view,name='updatetask'),

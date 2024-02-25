f6b79eeda396ad22eb3a5b3da075424f066d0151
Roopesh-N <roopeshnagineni123@outlook.com>
2024-02-25 17:55:15 -0500

    encrypted password commit

--- todoproject/todoApp/views.py
+++ todoproject/todoApp/views.py
@@ -10,12 +10,15 @@ def login_view(request):
     form=loginForm()
     if request.method=='POST':
         uname=request.POST['username']#entered username
-        epwd=request.POST['password']#entered password
+        enteredpwd=request.POST['password']#entered password
         form=loginForm(request.POST)
         if form.is_valid():
             try:
                 user = userModel.objects.get(username=uname)
-                if epwd != user.password:
+                encrypted_password=user.password
+
+                pwdstatus=passlib_encryption_verify(enteredpwd,encrypted_password)
+                if pwdstatus==False:
                     messages.error(request, "Incorrect password. Please try again.")
                 else:
                     slug=user.slug
@@ -31,10 +34,43 @@ def signup_view(request):
     if request.method=='POST':
         form=SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            firstname=request.POST['firstname']
+            lastname=request.POST['lastname']
+            username=request.POST['username']
+            password=request.POST['password']
+            epwd=passlib_encryption(password)
+            phone=request.POST['phone']
+            email=request.POST['email']
+            userModel.objects.create(firstname=firstname,lastname=lastname,username=username,password=epwd,phone=phone,email=email)
             messages.success(request,"Account created successfully, Please login")
             return redirect('login')
     return render(request,'todoapp/signup.html',{'form':form,})
+
+
+from passlib.hash import pbkdf2_sha256
+
+def passlib_encryption(raw_password):
+	"""
+	Here, Encryption is Using passlib Library.
+	"""
+	if raw_password:
+		encrypted = pbkdf2_sha256.hash(raw_password)
+	else:
+		encrypted = None
+	
+	return encrypted
+
+def passlib_encryption_verify(raw_password, enc_password):
+	""" 
+	@returns TRUE or FALSE 
+	"""
+	if raw_password and enc_password:
+		# verifying the password
+		response = pbkdf2_sha256.verify(raw_password, enc_password)
+	else:
+		response = None
+	
+	return response
 
 
 
